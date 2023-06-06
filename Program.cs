dbf41311f80c0e7373c9336bac40c57be2f70399
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-06-06 16:19:18 -0300

    EXERCÍCIO CONSLUÍDO

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("-=-Fluxograma da engenharia-=-\n");
+
+Console.Write("O objeto está se movendo?");
+    bool estasemovendo = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+Console.Write("Deveria?");
+    bool deveria = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+string mensagem;
+
+if (!estasemovendo)
+    {
+        if (!deveria) 
+        {
+            mensagem = "Ótimo.";
+        }
+        else
+        {
+            mensagem = "Use WD-40.";
+        }
+    }
+else 
+    {
+        if (deveria)
+        {
+            mensagem = "Ótimo.";
+        }
+        else
+        {
+            mensagem = "use Silver Tape.";
+        }
+    }
+
+Console.WriteLine();
+Console.Write(mensagem);
+
+
+
+
+
+
+
+
+
+
+
